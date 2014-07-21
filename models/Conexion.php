ab973c8255134c16cbb5c607b9b6265c83f0e4ba
Brenda Anabel <anabel.alva88@gmail.com>
2014-07-21 16:52:24 -0500

    Brenda Alvarado

--- models/Conexion.php
+++ models/Conexion.php
@@ -16,7 +16,7 @@ class Conexion {
         $this->db = ADONewConnection('mysql');
         $this->db->debug = false;
 			       //ip     user      pass    bd
-        $this->db->Connect('localhost','root','anabel12345','fifa');
+        $this->db->Connect('localhost','root','','fifa');
 
     }
     
